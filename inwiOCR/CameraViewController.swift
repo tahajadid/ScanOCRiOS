25b34c705783698cb149f535df5baaf3f49d0129
taha <taha.jadid>
2022-04-09 20:16:12 +0000

    First Commit

---
+++ inwiOCR/CameraViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CameraViewController.swift
+//  inwiOCR
+//
+//  Created by taha_jadid on 9/4/2022.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+}
--- inwiOCR/ViewController.swift
+++ inwiOCR/ViewController.swift
@@ -5,15 +5,234 @@
 //  Created by taha_jadid on 9/4/2022.
 //
 
+import MLImage
+import MLKit
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var openCameraTB: UIToolbar!
+    
+    /// An image picker for accessing the photo library or camera.
+    var imagePicker = UIImagePickerController()
 
+    /// An overlay view that displays detection annotations.
+    private lazy var annotationOverlayView: UIView = {
+      precondition(isViewLoaded)
+      let annotationOverlayView = UIView(frame: .zero)
+      annotationOverlayView.translatesAutoresizingMaskIntoConstraints = false
+      annotationOverlayView.clipsToBounds = true
+      return annotationOverlayView
+    }()
+    
+    /// A string holding current results from detection.
+    var resultsText = ""
+
+    // Image counter.
+    var currentImage = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        super.viewWillDisappear(false)
+        
+        imageView.image = UIImage(named: Constants.images[currentImage])
+        imageView.addSubview(annotationOverlayView)
+        NSLayoutConstraint.activate([
+          annotationOverlayView.topAnchor.constraint(equalTo: imageView.topAnchor),
+          annotationOverlayView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+          annotationOverlayView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+          annotationOverlayView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor),
+        ])
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+
+
     }
 
 
+    @IBAction func detectButton(_ sender: Any) {
+        self.resultsText = ""
+        detectTextOnDevice(image: imageView.image)
+    }
+    
+    @IBAction func openCamera(_ sender: Any) {
+        
+        guard
+          UIImagePickerController.isCameraDeviceAvailable(.front)
+            || UIImagePickerController
+              .isCameraDeviceAvailable(.rear)
+        else {
+          return
+        }
+        
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true)
+    }
+    
+    @IBAction func openLibraryPhoto(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
+    
+    /// Clears the results text view and removes any frames that are visible.
+    private func clearResults() {
+      self.resultsText = ""
+    }
+    
+    private func showResults() {
+      let resultsAlertController = UIAlertController(
+        title: "Résultats du scan cin",
+        message: nil,
+        preferredStyle: .actionSheet
+      )
+      resultsAlertController.addAction(
+        UIAlertAction(title: "Cancel", style: .destructive) { _ in
+          resultsAlertController.dismiss(animated: true, completion: nil)
+        }
+      )
+      resultsAlertController.message = resultsText
+      //resultsAlertController.popoverPresentationController?.barButtonItem = detectButton
+      resultsAlertController.popoverPresentationController?.sourceView = self.view
+      present(resultsAlertController, animated: true, completion: nil)
+      print(resultsText)
+    }
+    
+    /// Updates the image view with a scaled version of the given image.
+    private func updateImageView(with image: UIImage) {
+      let orientation = UIApplication.shared.statusBarOrientation
+      var scaledImageWidth: CGFloat = 0.0
+      var scaledImageHeight: CGFloat = 0.0
+      switch orientation {
+      case .portrait, .portraitUpsideDown, .unknown:
+        scaledImageWidth = imageView.bounds.size.width
+        scaledImageHeight = image.size.height * scaledImageWidth / image.size.width
+      case .landscapeLeft, .landscapeRight:
+        scaledImageWidth = image.size.width * scaledImageHeight / image.size.height
+        scaledImageHeight = imageView.bounds.size.height
+      @unknown default:
+        fatalError()
+      }
+      weak var weakSelf = self
+      DispatchQueue.global(qos: .userInitiated).async {
+        // Scale image while maintaining aspect ratio so it displays better in the UIImageView.
+        var scaledImage = image.scaledImage(
+          with: CGSize(width: scaledImageWidth, height: scaledImageHeight)
+        )
+        scaledImage = scaledImage ?? image
+        guard let finalImage = scaledImage else { return }
+        DispatchQueue.main.async {
+          weakSelf?.imageView.image = finalImage
+        }
+      }
+    }
+    
+    func process(_ visionImage: VisionImage, with textRecognizer: TextRecognizer?) {
+      weak var weakSelf = self
+      textRecognizer?.process(visionImage) { text, error in
+        guard let strongSelf = weakSelf else {
+          print("Self is nil!")
+          return
+        }
+        guard error == nil, let text = text else {
+          let errorString = error?.localizedDescription ?? Constants.detectionNoResultsMessage
+          strongSelf.resultsText = "Text recognizer failed with error: \(errorString)"
+          strongSelf.showResults()
+          return
+        }
+        // Blocks.
+        for block in text.blocks {
+
+          // Lines.
+          for line in block.lines {
+
+              
+            // Elements.
+            for element in line.elements {
+
+              strongSelf.resultsText += element.text + " "
+            }
+          }
+            strongSelf.resultsText += "\n"
+        }
+        
+        strongSelf.showResults()
+      }
+    }
+    
+}
+
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate {
+
+  func imagePickerController(
+    _ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+  ) {
+    // Local variable inserted by Swift 4.2 migrator.
+    let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+    clearResults()
+    if let pickedImage =
+      info[
+        convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)]
+      as? UIImage
+    {
+      updateImageView(with: pickedImage)
+    }
+    dismiss(animated: true)
+  }
+}
+
+
+/// Extension of ViewController for On-Device detection.
+extension ViewController {
+
+  // MARK: - Detection
+
+  /// Detects text on the specified image and draws a frame around the recognized text using the text recognizer.
+
+  /// - Parameter image: The image.
+  private func detectTextOnDevice(image: UIImage?) {
+    guard let image = image else { return }
+    let textRecognizer = TextRecognizer.textRecognizer()
+
+    // Initialize a `VisionImage` object with the given `UIImage`.
+    let visionImage = VisionImage(image: image)
+    visionImage.orientation = image.imageOrientation
+
+    self.resultsText += "Running Text Recognition...\n"
+    process(visionImage, with: textRecognizer)
+  }
+}
+
+private enum Constants {
+  // static let images = ["image_has_text.jpg"]
+  
+  static let images = ["natio.jpg"]
+  static let detectionNoResultsMessage = "No results returned."
+  static let failedToDetectObjectsMessage = "Failed to detect objects in image."
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+private func convertFromUIImagePickerControllerInfoKeyDictionary(
+  _ input: [UIImagePickerController.InfoKey: Any]
+) -> [String: Any] {
+  return Dictionary(uniqueKeysWithValues: input.map { key, value in (key.rawValue, value) })
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+private func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey)
+  -> String
+{
+  return input.rawValue
 }
 
